944c88cb845ad97ac51d23ee57fe85458843b9cc
darlim <85694595+darlim9141@users.noreply.github.com>
2024-04-25 16:37:49 -0500

    Update Recommender.py

--- Recommender.py
+++ Recommender.py
@@ -45,9 +45,61 @@ class Recommender:
     def get_available_genre_seeds(self):
         genres = self.sp.recommendation_genre_seeds()
         return genres
-
+    
     def get_json_result(self, favorite_tracks):
         details = self.get_track_details(favorite_tracks)
         track_ids = [d['ids'][0] for d in details.values() if d['ids']]
         recommended = self.sp.recommendations(seed_tracks=track_ids, limit=50)
         return recommended
+    
+    def get_track_info(self, track_ids):
+        tracks_info = []
+        for track_id in track_ids:
+            track_data = self.sp.track(track_id)
+            audio_features = self.sp.audio_features(track_id)[0]
+
+            track_info = {
+                'id': track_id,
+                'name': track_data['name'],
+                'artist': track_data['artists'][0]['name'],
+                'album': track_data['album']['name'],
+                'release_date': track_data['album']['release_date'],
+                'popularity': track_data['popularity'],
+                'duration_ms': track_data['duration_ms'],
+                'explicit': track_data['explicit'],
+                # Audio features
+                'danceability': audio_features['danceability'],
+                'energy': audio_features['energy'],
+                'key': audio_features['key'],
+                'loudness': audio_features['loudness'],
+                'mode': audio_features['mode'],
+                'speechiness': audio_features['speechiness'],
+                'acousticness': audio_features['acousticness'],
+                'instrumentalness': audio_features['instrumentalness'],
+                'liveness': audio_features['liveness'],
+                'valence': audio_features['valence'],
+                'tempo': audio_features['tempo'],
+                'time_signature': audio_features['time_signature']
+            }
+            tracks_info.append(track_info)
+        return tracks_info
+    
+
+    def get_track_ids(self, track_names):
+        track_ids = []
+        for track_name in track_names:
+            result = self.sp.search(q=track_name, limit=1, type='track')
+            if result['tracks']['items']:
+                track_id = result['tracks']['items'][0]['id']
+                track_ids.append(track_id)
+        return track_ids
+    
+    def clean_data(data):
+        # Remove duplicates
+        data = data.drop_duplicates()
+
+        # Fill or drop missing values
+        data['genre'] = data['genre'].fillna('Unknown')  # Fill missing genres with 'Unknown'
+        data.dropna(subset=['artist', 'track_name'], inplace=True)  # Drop rows where 'artist' or 'track_name' is missing
+
+        return data
